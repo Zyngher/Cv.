5bffb4b6c5e62e3ae927d0252d451cff5378adcb
Dani <d_danyi@yahoo.com>
2020-11-29 20:21:07 +0200

    New project

---
+++ Front page.js
@@ -0,0 +1,6 @@
+const itXp = document.querySelector('.it-xp');
+const workXp = document.querySelector('.work-xp');
+const hobbies = document.querySelector('.hobbies');
+const skils = document.querySelector('.skils');
+
+console.log(skils);
